6e0d39314702ebcbf625adda49ab33b03ef013cc
Evrim Öztamur <evrimoztamur@gmail.com>
2023-10-15 11:33:44 +0200

    Set up new package format

--- deploy-electron.sh
+++ deploy-electron.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 cd "maginet"
-wasm-pack build --target web --out-dir static/js/pkg --out-name maginet_aee75fc -- --features "deploy"
+wasm-pack build --target web --out-dir static/js/pkg --out-name maginet_aee75fc -- --features "deploy demo"
 
 cd -
-cp "maginet/html/itch.html" "index.html"
-cp -r "maginet/static" "."
+cp "maginet/html/itch.html" "src/index.html"
+cp -r "maginet/static" "src/static"
 
 npm run build
